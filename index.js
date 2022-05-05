a1902e0f8c6cb15b55a02ed246300640aa88ff6d
sohanur <sohanurl653@gmail.com>
2022-05-05 22:09:09 +0600

    jwt unstall

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const cors=require('cors');
+const jwt=require('jsonwebtoken')
 require('dotenv').config()
 const port=process.env.PORT ||4000;
 app.use(cors());
@@ -11,23 +12,21 @@ app.get("/",(req,res)=>{
 
 // get data from server
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DOCTOR_USER}:${process.env.DOCTOR_PASSWORD}@cluster0.hrgbo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-const run=async()=>{
-   try{
-    await client.connect(e=> console.log('device connected'))
-    const doctorCollection=client.db("doctor-web").collection("doctor-profile");
 
-    // get data
-    app.get("/services",async(req,res)=>{
-        const kuery={}
-        const cursor=doctorCollection.find(kuery)
-        const fishUser=await cursor.toArray();
-        res.send(fishUser)
-    })
-    // single data get
-    app.get('/services/:id',async(req,res)=>{
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://doctor-web:${process.env.DOCTOR_PASSWORD}@cluster0.hrgbo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+      await client.connect();
+      const doctorCollection=client.db('doctor-web').collection('doctor-profile')
+      app.get('/services',async(req,res)=>{
+          const query={}
+          const cursor=doctorCollection.find(query);
+          const user=await cursor.toArray();
+          res.send(user)
+      })
+      app.get('/services/:id',async(req,res)=>{
         const id = (req.params.id).trim();
         
         const query={_id :ObjectId(id)};
@@ -51,11 +50,44 @@ const run=async()=>{
         res.send(dlresult)
         console.log(uid)
     })
-   }
-   finally{
-    await client.close();
-   }
-}
-run().catch(console.dir);
+    
+    // post order,
+    app.post('/order',async(req,res)=>{
+        const order=req.body;
+        const checkout=await orderCollection.insertOne(order);
+        res.send(checkout)
+    
+    })
+    
+    // get order
+    app.get('/order',async(req,res)=>{
+        const search=req.query.email;
+    
+        const query={email:search};
+        const cursor=orderCollection.find(query);
+        const order=await cursor.toArray();
+        res.send(order)
+        console.log(search)
+    })
+//  token
+    app.post('/login',(req,res)=>{
+      const user=req.body;
+      const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN,{expiresIn:'1d'})
+      res.send({accessToken})
+  })
+  
+      console.log('connected')
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
 
-app.listen(port)
+
+app.listen(port);
+
+
+
+
+
+
